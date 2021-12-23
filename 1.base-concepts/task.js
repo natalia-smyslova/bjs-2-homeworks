d0aae12367353432e9cd7b196bb50bee288ec8a7
Natalia <madlynx@lenta.ru>
2021-12-23 16:28:55 +0300

    second commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -18,16 +18,31 @@ function solveEquation(a, b, c) {
 	}
   
   
-  function calculateTotalMortgage() {
-    let totalAmount=0;
-    let dateFrom = new Date();
-    let dateTo = new Date (document.getElementById("dateto").value);
-    let n=(dateTo.getFullYear()*12 + dateTo.getMonth())-(dateFrom.getFullYear()*12 + dateFrom.getMonth());
-    let S = document.getElementById("amount").value-document.getElementById("contribution").value; 
-    let P = (document.getElementById("percent").value/12)/100;
-    totalAmount=S*(P+ (P /(((1+P)**n)-1)));
-    totalAmount=totalAmount*n;
-    console.log (totalAmount.toFixed(2));
-    return totalAmount;
+  function getValueByID(ID)
+  {
+    return document.getElementById(ID).value;
+  }
+  
+  function show3()
+  { let Percent = getValueByID("percent");
+    console.log("Если это решение не то, о чем Вы говорили, прошу предоставить мне ссылку на пример как должно быть.");
+    let Contribution = getValueByID("contribution");
+    let Amount = getValueByID("amount");
+    let Date = getValueByID("dateto");
+    let result=calculateTotalMortgage(Percent, Contribution,Amount,Date);
   }
 
+  function calculateTotalMortgage(percent, contribution, amount, date) {
+  let totalAmount=0;
+  let dateFrom = new Date();
+  let dateTo = new Date (date);
+  let n=(dateTo.getFullYear()*12 + dateTo.getMonth())-(dateFrom.getFullYear()*12 + dateFrom.getMonth());
+  let S = amount - contribution;
+  let P = percent/12/100;
+  totalAmount=S*(P+ (P /(((1+P)**n)-1)));
+  totalAmount=totalAmount*n;
+  console.log (totalAmount.toFixed(2));
+  return totalAmount;
+    } 
+
+    
