7eb20e7f71a75e59322bb65ddef24b0091aa4fc8
Natalia <madlynx@lenta.ru>
2022-01-31 14:17:41 +0300

    fourth commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,64 +1,88 @@
-
-
 // Задание 1
-
 function getArrayParams(arr) {
+  // Найти кол-во всех чисел
   let elementsCount = arr.length;
+  //console.log("elementsCount: " + elementsCount);
+
+  //Найти сумму всех чисел и минимальное, максимальное значение
   let sum = 0;
   let max = arr[0];
-  let min = arr[0]; 
+  let min = arr[0];
   for (let i = 0; i < elementsCount; i = i + 1) {
+    //console.log(i);
     if (max < arr[i]) {
       max = arr[i];
     }
     if (min > arr[i]) {
       min = arr[i];
     }
+    //if (arr[i] > 100) {
+    //  console.log ("Ошибка: " + (i+1) + " элемент массива > 100");
+    //  return {min:0, max:0, avg:0};
+    //}
     sum = sum + arr[i];
   }
+  //console.log ("максимум " + max);
+  //console.log ("минимум " + min);
+  //console.log ("сумма: " + sum);
+
+  // Сравнить числа между собой
+  // Найти среднее арифметическое 
   let avarage = sum / elementsCount;
   avarage = avarage.toFixed(2);
   avarage = Number(avarage);
-  return {min:min, max:max, avg:avarage};
+
+  // Возвращаем результат
+  return { min: min, max: max, avg: avarage };
 }
+
+console.log(getArrayParams([-99, 99, 10]));
+console.log(getArrayParams([1, 2, 3, -100, 10]));
+console.log(getArrayParams([5]));
+
 // Задание 2
+
 function worker(arr) {
-  let sum = arr.reduce(function(sum, elem) {
-return sum + elem;
- }, 0);
+  console.log("arraySum");
+  let sum = 0;
+  for (let i = 0; i < arr.length; i = i + 1) {
+    sum = sum + arr[i];
+  }
   // Ваш код
   return sum
 }
 
 function makeWork(arrOfArr, func) {
-  let max=0;
-for (let i=0; i<arrOfArr.lenght; i++) {
-  console.log (func(arrOfArr[i]));
-  if (func(arrOfArr[i])>max);
-  max = func(arrOfArr[i]);
+  let max = 0;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let sumOfArr = func(arrOfArr[i]);
+    console.log(i + ') ' + sumOfArr);
+    if (sumOfArr > max)
+      max = sumOfArr;
+  }
+  return max;
 }
- return max;
-  // Ваш кода
-  // for ...
-  
-  
-}
+
 
 // Задание 3
-function worker2(arr) {
+function maxMinDiff(arr) {
   // Ваш код
 
-  let min=Infinity; //arr[0]
-  let max=-Infinity; //arr[0]
+  let min = arr[0];
+  let max = arr[0];
 
-  for (let i = 0; i <= arr.lenght; i++) {
-    if (i > max) {
-      max = i;
+  // hfkyg hil 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+      console.log ("max " + max);
     }
-    else if (i < min) {
-      min = i;
-    }
- } 
-let result = Math.abs(max-min);
-return result
+    if (arr[i] < min) {
+      min = arr[i];
+      console.log ("min: " + min);
+    } 
+  }
+
+  return Math.abs(max - min);
 }
+
