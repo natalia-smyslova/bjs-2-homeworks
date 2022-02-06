3a3845951b1666b97eb0460be0e84f76af70ff54
Natalia <madlynx@lenta.ru>
2022-02-06 17:41:59 +0300

    first commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,12 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  if (arr1.length > arr2.length) {
+    return arr1.every((elem, ind) => elem == arr2[ind]);
+  } else {
+    return arr2.every((elem, ind) => elem == arr1[ind]);
+  }
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  let resultArr = arr.filter(positiveNumber => positiveNumber > 0).filter(multiple => multiple % 3 === 0).map(generation => generation * 10);
+  return resultArr;
 }
